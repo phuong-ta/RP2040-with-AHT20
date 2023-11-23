151def39fe99de0ef960c1c8c6b4f7bde8a2aa17
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2023-11-23 20:47:56 +0200

    AHT Sensor done

--- AHT20/AHT20.h
+++ AHT20/AHT20.h
@@ -1,4 +1,3 @@
-// TemperatureSensor.h
 #ifndef AHT20_H
 #define AHT20_H
 
@@ -26,31 +25,3 @@ private:
 };
 
 #endif // AHT20_H
-
-
-
-/*
-#ifndef AHT20_H
-#define AHT20_H
-
-class AHT20 {
-private:
-    float tempReading = 0;
-    float humReading = 0;
-    const static int addr = 0x38;
-
-    bool checkCalibration();
-    void sensorInit();
-
-public:
-    AHT20();
-
-    void triggerMeasurement();
-    float getTemp();
-    float getTemp_f();
-    float getHumidity();
-    void init(int sdaPort, int sclPort);
-};
-
-#endif //_AHT20_H
-*/
